540cbe0408fed3abc83131d5026820a7f8e5b2d1
Jesse Bell <jkurtasbell@gmail.com>
2021-03-26 13:12:53 -0700

    Initial webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: 'node_modules',
+        use: 'babel-loader',
+      }
+    ]
+  }
+}
