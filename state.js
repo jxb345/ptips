736007b5e1c1fd1172235af275d5d91b6c1e9dc5
Jesse Bell <jkurtasbell@gmail.com>
2021-03-29 14:45:36 -0700

    List Parts of State

---
+++ state.js
@@ -0,0 +1,25 @@
+const Entries = [],
+
+const Entry = {
+  id: '',
+  author: '',
+  date: new Date(),
+  title: '',
+  content: '',
+  votes: 0,
+  tags: [],
+  comments: []
+},
+
+const User = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  entries: 0,
+  dateJoined: new Date(),
+},
+
+const Search = {
+  search: '',
+}
+
