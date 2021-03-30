8a03d8504e7b9220fed262cd28153c4d96085e30
Jesse Bell <jkurtasbell@gmail.com>
2021-03-30 14:47:41 -0700

    Add typescript Library

--- state.js
+++ state.js
@@ -1,15 +1,27 @@
 const Entries = [],
 
-const Entry = {
-  id: '',
-  author: '',
-  date: new Date(),
-  title: '',
-  content: '',
-  votes: 0,
-  tags: [],
-  comments: []
-},
+// const Entry = {
+//   id: '',
+//   author: '',
+//   date: new Date(),
+//   title: '',
+//   content: '',
+//   votes: 0,
+//   tags: [],
+//   comments: []
+// },
+
+interface Entry {
+  user: string,
+  title: string,
+  content: string,
+  votes: number,
+  tags: array,
+  comments: array
+
+}
+
+const entries: Entry[] = []
 
 const User = {
   firstName: '',
